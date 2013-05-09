223d5e0de3e2e034571ea786ff684c2dc2279814
Andreas Müller <amu@euve3499.vserver.de>
2013-05-09 17:10:00 +0000

    cleanup

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 var express = require('express');
+var http= require('http');
 var twitter = require('ntwitter');
-
 var app = express();
 var sys = require('sys');
 var oauth = require('oauth');
 var connect = require('connect');
+var secrets= require('./lib/secrets.js').Secrets;
 var mysql = require('mysql');
-var mysqlclient = mysql.createConnection({ database:'meters', user: 'root',  password: 'root'});
-var secrects= require('./lib/secrets').Secrets;
+var mysqlclient = mysql.createConnection({ database:'twitcare', user: secrets.db.user,  password: secrets.db.password});
+
+var passport = require('passport')
+  , util = require('util')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+
 function consumer() {
   return new oauth.OAuth(
     "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", 
-    _twitterConsumerKey, _twitterConsumerSecret, "1.0A", "http://twitcare.asmuelle.de/sessions/callback", "HMAC-SHA1");   
+    secrets._twitterConsumerKey, secrets._twitterConsumerSecret, "1.0A", "http://twitcare.asmuelle.de/sessions/callback", "HMAC-SHA1");   
 }
 
+passport.use(new TwitterStrategy({
+    consumerKey: secrets._twitterConsumerKey,
+    consumerSecret: secrets._twitterConsumerSecret,
+    callbackURL: "http://twitcare.asmuelle.de/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    findOrCreateUser(token, tokenSecret, profile, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var findOrCreateUser=function(token,tokenSecret,profile, cb) {
+  mysqlclient.query("SELECT * from user where tid=?",[profile.id], function(err,rows) {
+    if(rows.length==0) {
+      var p=profile._json;
+      mysqlclient.query("INSERT into user (tid, screenname,name,description, url, img_url,lang,location,timezone,utcoffset) values(?,?,?,?,?,?,?,?,?,?)",[p.id,p.username,p.name,p.description,p.url,p.profile_image_url,p.lang,p.location,p.timezone,p.utc_offset],function(err){console.log(err);});
+    }
+  });
+  
+  profile.token=token;
+  profile.tokenSecret=tokenSecret;
+  cb(null,profile);
+};
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -22,12 +60,14 @@ app.configure(function(){
   app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(express.session({ secret: secrets.session_phrase }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
   app.use(function(err, req, res, next){
     res.status(err.status || 500);
-    console.log(["server error",err,req]);
+    console.log(["server error",err]);
     res.render('500', { title:"He is dead Jim!", error: err,user:req.user });
   });
   app.use(function(req, res, next){
@@ -39,53 +79,135 @@ app.configure(function(){
   });
 });
 
-app.get('/sessions/connect', function(req, res){
-  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-    if (error) {
-      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
-    } else {  
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
-    }
-  });
+app.get('/', ensureAuthenticated, function(req,res) {
+ res.sendfile(__dirname + '/public/index.html');
 });
 
-app.get('/sessions/callback', function(req, res){
-  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
-    } else {
-      req.session.oauthAccessToken = oauthAccessToken;
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-      res.redirect(req.session.target);
-    }
-  });
+app.get('/login', function(req,res) {
+ res.sendfile(__dirname + '/public/signin.html');
 });
 
-app.get('/twitcare', ensureTwitterAuthorized, function(req, res){
-  var twit = new twitter({
+app.get('/oauth',
+  passport.authenticate('twitter'),
+  function(req, res){
+  });
+
+req.on("error",function(err) {console.log(err)});
+req.end();
+});
+
+app.put('/unshortenurls', function(request,response) {
+ var retval={};
+ Object.keys(request.body).forEach(function(u,i) { 
+  var url=u.slice(7);
+  var host=url.slice(0,url.indexOf("/"));
+  var path=url.slice(url.indexOf("/"));
+  var req = http.request({hostname:host,path:path ,port:80,method:"HEAD"}, function(res) {
+   if(res.statusCode===301) {
+      console.log(res.headers.location);
+      retval[res.headers.location]=request.body[u];
+   } else {retval[u]=request.body[u];}
+   if(i==Object.keys(request.body).length-20)  response.send(retval);
+
+  });
+
+ req.on("error",function(err) {console.log(err)});
+
+ req.end();
+ });
+});
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    var u=req.user._json;
+    mysqlclient.query("INSERT into user_log(tid, nstatus, nfriends, nfollowers,nlist,nfav,sdate) values(?,?,?,?,?,?,?)",
+     [u.id, u.statuses_count, u.friends_count,u.followers_count, u.listed_count,u.favourites_count,new Date(u.status.created_at).getTime()],
+      function(err,x) {
+       if(err) console.log(err);});
+       res.redirect('/');
+  });
+
+var getTwitterAPI=function(req) {
+ return new twitter({
   consumer_key: secrets._twitterConsumerKey,
   consumer_secret: secrets._twitterConsumerSecret,
-  access_token_key: req.session.oauthAccessToken,
-  access_token_secret: req.session.oauthAccessTokenSecret
+  access_token_key: req.user.token,
+  access_token_secret: req.user.tokenSecret
 });
-twit
-  .verifyCredentials(function (err, data) {
-    console.log(data);
-    res.render('twitcare', { data:data});
+
+}
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/bd',  function(req, res){
+ getTwitterAPI(req)
+  .getHomeTimeline({count:200},function (err, data) {
+    res.send(err||{tl:data,user:req.user._json});
   });
 });
 
-app.listen(3001);
-console.log('Listening on port 3001');
+app.get('/mentions', function(req, res){
+getTwitterAPI(req)
+  .getMentions({count:200},function (err, data) {
+    res.send(err||data);
+  });
+});
 
-function ensureTwitterAuthorized(req,res,next) {
- if(req.session.oauthRequestToken) {return next()}
- else {
-  req.session.target=req.url;
-  res.redirect('/sessions/connect')
- }
+app.get('/retweets', function(req, res){
+ getTwitterAPI(req)
+  .getRetweetsOfMe({count:200},function (err, data) {
+    res.send(err||data); 
+  });
+});
+
+app.get('/search', function(req, res){
+ getTwitterAPI(req)
+  .search("javascript OR nodejs",{count:100,include_entities:true, result_type:"popular"},function (err, data) {
+    res.send(err||data);
+  });
+});
+
+app.get('/follow/:id', function(req,res) {
+
+});
+
+app.get('/unfollow/:id', function(req,res) {
+});
+
+app.get('/favourize/:id', function(req,res) {
+});
+
+app.get('/retweet/:id', function(req,res) {
+ getTwitterAPI(req).retweet(req.params.id, function(err,data) {
+ 
+ });
+});
+
+app.get('/retweets/:id', function(req,res) {
+ getTwitterAPI(req).getRetweets(req.params.id, function(err,data) {
+   res.send(err||data);
+ });
+});
+
+app.get('/reply/:id', function(req,res) {
+});
+
+app.get('/unfavourize/:id', function(req,res) {
+});
+
+app.get('/tweet', function(req,res) {
+ getTwitterAPI(req).updateStatus(req.query.message, function(err,data) {
+   res.send(err||data);
+ });
+});
+
+app.listen(3030);
+console.log('Listening on port 3030');
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
 }
-
-
